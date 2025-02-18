09f747491f4d28ea32406f37a3f478050af10fb6
OTonGitHub <purple@daraknet.cc>
2025-02-18 17:55:49 +0500

    fix  user perm config, refactor templates

--- main.tf
+++ main.tf
@@ -26,34 +26,38 @@ data "xenorchestra_pool" "deepblue" {
 resource "xenorchestra_cloud_config" "cc" {
   name = var.CC_NAME
   template = templatefile("cloud_config.tftpl", {
-    username   = var.CC_UNAME
+    hostname   = var.VM_HOSTNAME,
+    username   = var.CC_UNAME,
     nic        = var.CC_NICNAME,
     ip_address = var.VM_IP_ADDRESS,
     gateway    = var.VM_GATEWAY,
     ssh_key    = var.SSH_PUB_KEY,
-    password   = var.CC_PASWD
+    password   = var.CC_PASWD_HASH,
+    ansible_user = var.ANSIBLE_VM_USER,
+    ansible_key  = var.ANSIBLE_PUB_KEY
   })
 }
 
 resource "xenorchestra_vm" "kraken" {
-  name_label    = var.VM_NAME
-  template      = var.UUID_TEMPLATE
-  auto_poweron  = true
+  name_label       = var.VM_NAME
+  name_description = var.VM_DESCRIPTION
+  template         = var.UUID_TEMPLATE
+  auto_poweron     = true
 
-  cpus          = var.VM_CPUs
-  memory_max    = var.VM_MEMORY_GB * 1024 * 1024 * 1024
-  cloud_config  = xenorchestra_cloud_config.cc.template
+  cpus         = var.VM_CPUs
+  memory_max   = var.VM_MEMORY_GB * 1024 * 1024 * 1024
+  cloud_config = xenorchestra_cloud_config.cc.template
 
   affinity_host = data.xenorchestra_pool.deepblue.master
 
   disk {
-    sr_id       = var.UUID_SR
-    name_label  = var.VM_DISK_NAME
-    size        = var.VM_DISK_SIZE * 1024 * 1024 * 1024
+    sr_id      = var.UUID_SR
+    name_label = var.VM_DISK_NAME
+    size       = var.VM_DISK_SIZE_GB * 1024 * 1024 * 1024
   }
 
   network {
-    network_id  = var.UUID_NIC
+    network_id = var.UUID_NIC
   }
 
   tags = [
--- variables.tf
+++ variables.tf
@@ -1,108 +1,130 @@
-variable "XOA_URL" { # Must be ws or wss
-  description = "The URL to Xen Orchestra instance (wss://10.10.0.69)"
+variable "XOA_URL" {  # Must be ws or wss
+  description = "Xen Orchestra URL (wss://10.20.10.69)."
   type        = string
 }
 
-variable XOA_TOKEN {
-  description = "Token generated from XOA, can from XO-UI or XO-CLI"
+variable "XOA_TOKEN" {
+  description = "Xen Orchestra token (from XO-UI or XO-CLI)."
   type        = string
   sensitive   = true
 }
 
 variable "XOA_USER" {
-  description = "Username for Xen Orchestra"
+  description = "Xen Orchestra username."
   type        = string
 }
 
 variable "XOA_PASSWORD" {
-  description = "Password for Xen Orchestra"
+  description = "Xen Orchestra password."
   type        = string
   sensitive   = true
 }
 
 variable "CC_NAME" {
-  description = "Name of Cloud-init config, saved to XOA"
+  description = "Cloud-init config name (saved in XOA)."
   type        = string
 }
 
 variable "CC_UNAME" {
-  description = "Cloud-init username to set on VM"
+  description = "Cloud-init username for the VM."
   type        = string
 }
 
-variable "CC_PASWD" {
-  description = "Cloud-init user password for sudo access"
+variable "CC_PASWD_HASH" {
+  description = "SHA-512 password hash for sudo (use 'openssl passwd -6')."
   type        = string
   sensitive   = true
 }
 
 variable "CC_NICNAME" {
-  description = "Cloud-init VM image NIC name"
+  description = "NIC name for the Cloud-init VM image."
   type        = string
 }
 
 variable "VM_IP_ADDRESS" {
-  description = "IPv4 Address of the VM, DHCP4 Must be desabled"
+  description = "VM IPv4 address (DHCP4 must be disabled)."
   type        = string
 }
 
 variable "VM_GATEWAY" {
-  description = "IPv4 gateway for the VM, check VLAN"
+  description = "VM IPv4 gateway (check VLAN)."
   type        = string
 }
 
 variable "POOL_NAME" {
-  description = "The name of your XCP-ng pool"
+  description = "XCP-ng pool name."
   type        = string
   default     = "GCX-DeepBlue"
 }
 
 variable "UUID_TEMPLATE" {
-  description = "UUID of the template to terraform"
+  description = "Template UUID for Terraform."
   type        = string
 }
 
 variable "UUID_NIC" {
-  description = "UUID of the physical NIC from the pool"
+  description = "Physical NIC UUID from the pool."
   type        = string
 }
 
 variable "UUID_SR" {
-  description = "UUID of the storage resporitory for VM to instll in"
+  description = "Storage repository UUID for VM installation."
+  type        = string
+}
+
+variable "VM_HOSTNAME" {
+  description = "Hostname for the VM, created by cloud-init."
   type        = string
 }
 
 variable "VM_NAME" {
-  description = "The name label for the new VM"
+  description = "Label for the new VM."
+  type        = string
+}
+
+variable "VM_DESCRIPTION" {
+  description = "Purpose of VM, viewed in XOA"
   type        = string
 }
 
 variable "VM_CPUs" {
-  description = "Number of vCPUs for the VM"
+  description = "Number of VM vCPUs."
   type        = number
   default     = 2
 }
 
 variable "VM_MEMORY_GB" {
-  description = "Memory for the VM (in GB)"
+  description = "VM memory in GB."
   type        = number
   default     = 2
 }
 
-variable "VM_DISK_SIZE" {
-  description = "Disk size for the VM (in GB)"
+variable "VM_DISK_SIZE_GB" {
+  description = "VM disk size in GB."
   type        = number
   default     = 10
 }
 
 variable "VM_DISK_NAME" {
-  description = "Name for VM root disk"
+  description = "Name of the VM's root disk."
   type        = string
   default     = "root-disk"
 }
 
 variable "SSH_PUB_KEY" {
-  description = "public SSH key for user in cloud-init for VM"
+  description = "Public SSH key for the VM (Cloud-init)."
+  type        = string
+  sensitive   = true
+}
+
+variable "ANSIBLE_PUB_KEY" {
+  description = "Public SSH key for Ansible to manage VM (Cloud-init)."
+  type        = string
+  sensitive   = true
+}
+
+variable "ANSIBLE_VM_USER" {
+  description = "Username to be used by Ansible to manage VM."
   type        = string
   sensitive   = true
 }
